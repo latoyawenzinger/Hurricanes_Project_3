a835aae41de77b7877768220f89416d81ca30857
Latoya Wenzinger <lnorman1016@gmail.com>
2023-04-12 18:42:04 -0400

    updated html/javascript

--- static/logic.js
+++ static/logic.js
@@ -1,51 +1,116 @@
 function init() {
     d3.csv('/Resources/top_10.csv').then(data => {
-            console.log(data)
+        console.log(data)
 
-            var dropDown = d3.select('#selDataset');
+        var dropDown = d3.select('#selDataset');
             // fetch hurricanes
             
-            var hurricaneNames = []
-            for (let i = 0; i < data.length; i++) { 
-                hurricaneNames.push(data[i].Name)
-            };
-            // remove duplicates
-            var uniqueNames = [...new Set(hurricaneNames)];
-            console.log(uniqueNames)
-            // assign hurricanes to the dropdown menu 
-            uniqueNames.map((id) => dropDown
+        var hurricaneNames = []
+        for (let i = 0; i < data.length; i++) { 
+            hurricaneNames.push(data[i].Name)
+        };
+        // remove duplicates
+        var uniqueNames = [...new Set(hurricaneNames)];
+    
+        // assign hurricanes to the dropdown menu 
+        uniqueNames.map(name => dropDown
             .append('option')
-            .text(id)
-            .property('value', id));
-            })
-        }
+            .text(name)
+            .property('value', name));
+
+        hurricanePanel(uniqueNames[0])
+    })
+}
 
 init();
 
-//function hurricanePanel(name) {
-    d3.csv('/Resources/top_10.csv').then(data=> {
+function hurricanePanel(name) {
+    d3.csv('/Resources/top_10snapshot.csv').then(data=> {
         
         //select panel element
         var panel = d3.select('#hurricaneData');
         // clear previous data
         panel.html('');
-        
-        //load metadata for panel
-        var date = newDate(data[0].Date).toTimeString();
-        console.log(date)
 
-        //filter/find data for choosen ID
-        var metadataID = metadata.filter(patientInfo => patientInfo.id == id)[0];
+        //filter/find data for choosen name
+        var hurricaneName = data.filter(Name => Name.Hurricane == name)[0];
     
         // append each key and value to demographics panel
-        Object.entries(metadataID).map(([key, value]) => panel
+        Object.entries(hurricaneName).map(([key, value]) => panel
             .append('p')
             .text(`${key}: ${value}`))    
     });
-//};
+};
 
 
-function createMap() {};
+function optionChanged(value) {
+    hurricanePanel(value)
+  };  
+  
+function buttonClicked(value) {
+
+    // Use D3 to retrieve all of the data
+    d3.csv('/Resources/Damage_Numbers_Updated.csv').then((data) => {
+        d3.select('#selDataset').html('');
+        d3.select('#hurricaneData').html('');
+
+        // Sort the data by rank in descending order
+        data = data.sort((a, b) => b['Rank'] - a['Rank']);
+
+        // Filter the top 10 hurricanes based on rank
+        let filteredData = data.slice(0, 10);
+
+        // Get the damage and year data for the selected hurricanes
+        let damageData = filteredData.map((d) => d['Damage']);
+        let yearData = filteredData.map((d) => d['Year']);
+
+        // Get the hurricane name and category data for the selected hurricanes
+        let names = filteredData.map((d) => d['Hurricane Name']);
+        let categories = filteredData.map((d) => d['Category']);
+
+        // Set up the trace for bubble chart
+        let trace1 = {
+            x: yearData,
+            y: damageData,
+            mode: "markers",
+            marker: {
+                size: damageData,
+                sizemode: 'diameter',
+                sizeref: 100000,
+                color: categories,
+                colorscale: 'Bluered',
+                colorbar: {
+                    title: 'Category',
+                    thickness: 20,
+                    ticks: 'outside'
+                }
+            },
+            text: filteredData.map((d) => `Name: ${d['Hurricane Name']}<br>Category: ${d['Category']}<br>Damage: $${d['Damage']} billion`)
+        };
+
+        // Set up the layout
+        let layout = {
+            title: "Top 10 Hurricanes by Rank",
+            hovermode: "closest",
+            xaxis: {title: "Year"},
+            yaxis: {title: "Damage (in billions USD)"},
+            annotations: names.map((name, i) => ({
+                x: yearData[i],
+                y: damageData[i],
+                text: name,
+                showarrow: false
+            }))
+        };
+
+        // Call Plotly to plot the bubble chart
+        Plotly.newPlot("bubble", [trace1], layout);
+    });
+};
+    
+
+
+
+
 
 
 
